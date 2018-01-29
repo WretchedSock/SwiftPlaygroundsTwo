45c237d09670e131c03330a2b3e0818554a28d78
WretchedSock <35237456+WretchedSock@users.noreply.github.com>
2018-01-29 10:28:54 -1000

    Going a bit ahead of the class with the else statement. Just waiting for now

--- PlaygroundTwo.playground/Contents.swift
+++ PlaygroundTwo.playground/Contents.swift
@@ -3,3 +3,12 @@
 import UIKit
 
 var str = "Hello, playground."
+
+var goodGrade = "I'm not doing very well"
+if goodGrade == "I've been doing all my work"
+{
+    print("I should be getting an A at this point")
+}
+else { print("Cry your heart out")}
+
+
