5927e79d0f77beeb267b4d08cd52059663cb5988
WretchedSock <35237456+WretchedSock@users.noreply.github.com>
2018-01-29 10:34:10 -1000

    Finished Part 3, #2 and Part 4

--- PlaygroundTwo.playground/Contents.swift
+++ PlaygroundTwo.playground/Contents.swift
@@ -4,11 +4,20 @@ import UIKit
 
 var str = "Hello, playground."
 
-var goodGrade = "I'm not doing very well"
+var goodGrade = "I've been doing all my work"
 if goodGrade == "I've been doing all my work"
 {
     print("I should be getting an A at this point")
 }
-else { print("Cry your heart out")}
+    //evaluate the conditional so it's false
+    
+    //Part 3, #2
+    if goodGrade == "I'm not doing very well"
+    {print("I'm not doing well in this class") }
+else { print("Good job!")} //Just chilling, waiting
 
+//Part 4
+if goodGrade == "I've been doing all my work"
+{print("I'm doing well in this class")}
+else {print("I'm not doing too well") }
 
